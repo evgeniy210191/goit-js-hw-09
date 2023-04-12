d7ef1bf1f63311c031d336adac3bf3c3927d215a
evgeniy210191 <bees127@ukr.net>
2023-04-12 18:33:56 +0300

    first commit with error

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,17 @@
+import { getRandomHexColor, intervalFn, body, start, stop } from './fn01/api';
+
+function startInterval(event) {
+  if (!intervalFn) {
+    intervalFn = setInterval(() => {
+      body.style.background = getRandomHexColor();
+      console.log(intervalFn);
+    }, 1000);
+  }
+}
+function stopInterval(event) {
+  intervalFn = null
+  clearInterval(intervalFn);
+}
+
+start.addEventListener('click', startInterval);
+stop.addEventListener('click', stopInterval);``
---
+++ src/js/fn01/api.js
@@ -0,0 +1,8 @@
+export function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+export const [body, start, stop] = ['body', '[data-start]', '[data-stop]'].map(
+  item => document.querySelector(item)
+);
+
+export let intervalFn = null;
